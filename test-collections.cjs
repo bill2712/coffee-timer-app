b33e077a127865cf213f1ef37aecc386b627bbc2
bill2712 <bill27122002@gmail.com>
2026-06-12 18:10:04 -0700

    feat(ui): add premium onboarding guide page with 13 locale translations

--- test-collections.cjs
+++
@@ -1,7 +0,0 @@
-const { parse } = require('path');
-const fs = require('fs');
-
-const dir = fs.readdirSync('src/content/blog/zh-TW');
-console.log(dir);
-
-// This script can't access Astro's `getCollection` directly, so we'll run a minimal Astro test instead.
