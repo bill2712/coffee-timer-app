917339d84c08d1e2268656441d31311927f315b0
bill2712 <bill27122002@gmail.com>
2026-09-01 08:00:13 -0700

    Updates

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'barista-flow-v5';
-const APP_SHELL = ['/', '/zh-TW/', '/en/', '/zh-TW/training/', '/en/training/', '/zh-TW/tools/grinder-calibration/', '/en/tools/grinder-calibration/', '/manifest.json', '/favicon.svg'];
+const CACHE_NAME = 'barista-flow-v6';
+const APP_SHELL = ['/', '/zh-TW/', '/en/', '/zh-TW/training/', '/en/training/', '/zh-TW/beans/', '/en/beans/', '/zh-TW/insights/', '/en/insights/', '/zh-TW/tools/grinder-calibration/', '/en/tools/grinder-calibration/', '/manifest.json', '/favicon.svg'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
